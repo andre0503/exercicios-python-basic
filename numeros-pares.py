831165121d28a94fc51a565baa0e05c85602e96a
André Luís <andreemoraees.borges@gmail.com>
2019-11-11 21:25:24 -0300

    exercicios 11/11

---
+++ numeros-pares.py
@@ -0,0 +1,7 @@
+numero = int(input("coloque um número inteiro: "))
+numero = int(numero)
+
+if numero % 2 == 0:
+    print("O número ", numero, "é par")
+else:
+    print("O numero ", numero, "é impar")
